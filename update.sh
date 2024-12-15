f5e2782be6d26b5ebf9b45f5a3701b088a325957
bandithedoge <bandithedoge@protonmail.com>
2024-12-15 14:11:27 +0100

    ci: use github token to avoid getting rate limited

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ function fetch() {
     nix-prefetch-docker --image-name "devkitpro/$1" --json --quiet >"sources/$1.json"
 }
 
-fd flake.nix -x nix flake update --flake "{//}"
+fd flake.nix -x nix flake update --flake "{//}" --option access-tokens "github.com=$GITHUB_TOKEN"
 
 mkdir -p sources
 
